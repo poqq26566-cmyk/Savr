97c015f11ffe00c301dab96eac30ed20c5df8883
poqq26566-cmyk <poqq26566@gmail.com>
2026-07-29 10:32:30 +0800

    Update BottomAppBarItem.kt

--- app/src/main/java/com/zarnth/savr/presentation/root/BottomAppBarItem.kt
+++ app/src/main/java/com/zarnth/savr/presentation/root/BottomAppBarItem.kt
@@ -3,25 +3,25 @@ package com.zarnth.savr.presentation.root
 import com.zarnth.savr.R
 
 data class BottomAppBarItem(
-    val title: String,
+    val titleRes: Int,  // 改为资源ID
     val icon: Int,
     val iconFilled: Int
 )
 
-val bottomAppBarItems = listOf<BottomAppBarItem>(
+val bottomAppBarItems = listOf(
     BottomAppBarItem(
-        title = "Home",
+        titleRes = R.string.nav_home,
         icon = R.drawable.home_icon,
         iconFilled = R.drawable.home_icon_filled
     ),
     BottomAppBarItem(
-        title = "Collections",
+        titleRes = R.string.nav_collections,
         icon = R.drawable.bookmark_one,
         iconFilled = R.drawable.bookmark_fill
     ),
     BottomAppBarItem(
-        title = "Settings",
+        titleRes = R.string.nav_settings,
         icon = R.drawable.setting_icon,
         iconFilled = R.drawable.setting_icon_filled
-    ),
+    )
 )
